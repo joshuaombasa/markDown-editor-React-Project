1d0e5115fc89e3a6793109e0376e90982bc5ab99
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-30 13:07:17 +0300

    updated jsx

--- src/Notes.jsx
+++ src/Notes.jsx
@@ -2,7 +2,7 @@
 export default function Notes() {
     return (
         <div className="notes--container">
-            <nav>
+            <nav className="notes--nav">
                 <h2 className="notes--header">Notes</h2>
                 <button className="add--note-btn">+</button>
             </nav>
