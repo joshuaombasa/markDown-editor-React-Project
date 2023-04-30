57307af48890f9d801e27b433a6e5d535f3e666e
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-30 13:29:30 +0300

    updated jsx

--- src/Notes.jsx
+++ src/Notes.jsx
@@ -1,11 +1,20 @@
 
-export default function Notes() {
+export default function Notes(props) {
+
+    const notesJsx = props.notesData.map((note) => {
+        return <p key={`${note}${note.length}`}>{note}</p>
+    })
+
+
+
     return (
         <div className="notes--container">
             <nav className="notes--nav">
                 <h2 className="notes--header">Notes</h2>
-                <button className="add--note-btn">+</button>
+                <button className="add--note-btn" onClick={props.addNote}>+</button>
             </nav>
+
+            {notesJsx}
         </div>
     )
 }
