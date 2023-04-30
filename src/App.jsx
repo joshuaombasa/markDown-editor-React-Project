0f8fef8f6eb9dbd52cb895c5b2be9448a264e8ce
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-30 13:07:02 +0300

    updated jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,21 @@ import './App.css'
 
 function App() {
 
+  const [notesData, setnotesData] = React.useState(['Note 1', 'Note 2'])
+
+  
+
+  function addNote() {
+    console.log('clickded')
+  }
+
   return (
     <div>
       <Split sizes={[30, 50]} direction="horizontal" className='split--container' >
-        <Notes />
+        <Notes 
+        notesData={notesData}
+        addNote={addNote}
+        />
         <div className='panel--2'>Panel 2</div>
       </Split>
 
