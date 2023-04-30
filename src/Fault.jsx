be9c3d2401af0aeb6a317bdf24d8df3dbddf482f
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-30 14:39:23 +0300

    updated jsx

---
+++ src/Fault.jsx
@@ -0,0 +1,8 @@
+export default function Fault() {
+    return (
+        <section className="fault--container">
+            <h3 className="fault--head">Failed breaks</h3>
+            <p className="fault--desctiption">Users are reposrting faults when in highspeed, brakes dont work.</p>
+        </section>
+    )
+}
