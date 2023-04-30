3c6427265277560b14d89671c337dd3632caaba8
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-04-30 14:58:32 +0300

    updated jsx

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -9,20 +9,42 @@ export default function Editor() {
         }
     )
 
+    function handleChange(event) {
+        const {name, value} = event.target
+        setfaultData((prevfaultData) => {
+            return {
+                ...prevfaultData,
+                [name] : value
+            }
+        })
+    }
+
+    
+
+    function submitForm(event) {
+        event.preventDefault() 
+        console.log(faultData)
+    }
+
     return (
         <section className="editor--container">
             <h2 className="editor-section-header">Add Fault</h2>
-            <form action="" className="fault-edit-form">
+            <form onSubmit={submitForm} className="fault-edit-form">
             <input
                 type="text"
                 className="fault--header"
                 name="faultheader"
+                onChange={handleChange}
+                value={faultData.faultheader}
             />
-            <input
+            <textarea
                 type="text"
                 className="fault--decsription"
                 name="faultdecsription"
+                onChange={handleChange}
+                value={faultData.faultdecsription}
             />
+            <button className="submit-btn">Submit</button>
         </form>
         </section>
     )
